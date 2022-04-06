5e9e0954170af485af846d08a0788ce34671659e
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-06 18:15:53 -0300

    adiciona exercicio 2 resolvido com nome de prog2 e tambem corrige exercicio 1 que tinha sido criado com nome de prog2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/prog01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/prog01.js
@@ -1,2 +1,5 @@
 const a = 2;
 const b = 3;
+
+adicao = a + b;
+console.log(adicao);
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/prog02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/prog02.js
@@ -1,5 +1,5 @@
 const a = 2;
 const b = 3;
 
-adicao = a + b;
-console.log(adicao);
+subtracao = a - b;
+console.log(subtracao);
