2368309463bc333da08c4bf4b3188b2f8b7f31ab
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-07 18:32:53 -0300

    Adiciona resolução do exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -55,3 +55,22 @@ for (let i = 0; i < numbers.length; i++) {
   }
 }
 console.log(bigger);
+
+// exercicio 6
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let qtdimpar = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  let comparador = numbers[index] % 2;
+  if (comparador !== 0) {
+    qtdimpar += 1;
+  }  
+}
+
+if (qtdimpar > 0) {
+  console.log(qtdimpar);
+}
+else {
+  console.log('nenhum valor impar encontrado')
+}
