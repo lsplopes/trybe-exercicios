f8862d481a64eaf1901c9d45ef5c30febe085305
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-07 17:31:28 -0300

    Adiciona pasta dia 4 e resolução dos exercicios do dia de 0 a 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -0,0 +1,57 @@
+// exercicio 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+// exercicio 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  soma += 0 + numbers[i];
+}
+console.log(soma);
+
+//exercicio 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  soma += 0 + numbers[i];
+}
+let media = soma / numbers.length;
+console.log(media);
+
+// exercicio 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  soma += 0 + numbers[i];
+}
+let media = soma / numbers.length;
+
+if (media > 20) {
+  console.log("valor maior que 20");
+}
+else {
+  console.log("valor menor ou igual a 20");
+}
+
+// exercicio 5
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let bigger = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (bigger < numbers[i]) {
+    bigger = numbers[i];
+  }
+}
+console.log(bigger);
