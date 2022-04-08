cea0fa037a95dc450708b64eac942da7879e6828
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-08 15:02:58 -0300

    Resolve o exercicio 3 do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -24,19 +24,42 @@
 //##############################################################3
 // EXERCICIO 2 - Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'Tryber';
-let arrayword = [];
-for (let letters of word) {
-  arrayword.push(letters);
+//let word = 'Tryber';
+//let arrayword = [];
+//for (let letters of word) {
+//  arrayword.push(letters);
+//}
+//console.log(arrayword);
+//
+//arrayword.reverse();
+//
+//console.log(arrayword);
+//
+//let reverseword = arrayword.toString();
+//
+//reverseword = reverseword.replace(/,/g, '');
+//
+//console.log(reverseword);
+
+// EXERCICIO 3 - Considere o array de strings abaixo e escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let bigger = 0;
+let biggerword;
+let smallestword;
+
+for (index = 0; index < array.length; index += 1) {
+  if (array[index].length > bigger) {
+    bigger = array[index].length;
+    biggerword = array[index];
+  }
 }
-console.log(arrayword);
-
-arrayword.reverse();
-
-console.log(arrayword);
-
-let reverseword = arrayword.toString();
-
-reverseword = reverseword.replace(/,/g, '');
-
-console.log(reverseword);
+let smallest = bigger;
+for (index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallest) {
+    smallest = array[index].length;
+    smallestword = array[index];
+  }
+}
+console.log("maior palavra é: " + biggerword);
+console.log("menor palavra é: " + smallestword);
