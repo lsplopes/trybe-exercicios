5e2091788b324840a1a6318ca46ae341f53abb0c
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-11 18:52:50 -0300

    Resolve os exercicios 5, 6, 7 e 8 da parte 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -18,6 +18,55 @@ info.recorrente = 'Sim';
 //}
 
 //EXERCICIO 4
-for (let keys in info) {
-  console.log(info[keys]);
+//for (let keys in info) {
+//  console.log(info[keys]);
+//};
+
+//EXERCICIO 5
+//let info2 = {
+//  personagem: 'Tio Patinhas',
+//  origem: 'Christmas on Bear Mountain',
+//  nota: "Dell's Four Color Comics #178, O último MacPatinhas",
+//  recorrente: 'Sim',
+//};
+//
+//for (let keys in info, info2) {
+//  if (keys === 'recorrente' && info[keys] === 'Sim' && info2[keys] === 'Sim') {
+//    console.log('Ambos recorrente');
+//  }
+//  else {
+//  console.log(info[keys] + ' e ' + info2[keys]);
+//  }
+//}
+
+//EXERCICIO 6
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
+
+//console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+//EXERCICIO 7
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+//console.log(leitor);
+
+//EXERCICIO 8
+
+
+
+console.log(leitor.nome + ' tem ' +leitor.livrosFavoritos.length + ' livros favoritos');
