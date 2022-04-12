ad775c394c21ffc5251323e78875f3b1a1fdac29
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-11 21:35:17 -0300

    resove os exercicio 2, 3 e 4 da parte 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios2.js
@@ -1,17 +1,73 @@
 // EXERCICIO 1
 
-function verificapalindrome(palavra) {
-  let arrayDaPalavra = palavra.split('');
-  let palavraReverse = arrayDaPalavra.reverse();
-  let palindromePalavra = palavraReverse.join('');
-  if (palindromePalavra === palavra) {
-    console.log(true);
+//function verificapalindrome(palavra) {
+//  let arrayDaPalavra = palavra.split('');
+//  let palavraReverse = arrayDaPalavra.reverse();
+//  let palindromePalavra = palavraReverse.join('');
+//  if (palindromePalavra === palavra) {
+//    console.log(true);
+//  }
+//  else {
+//    console.log(false);
+//  }
+//}
+//
+//verificapalindrome('lucas');
+//verificapalindrome('arara');
+
+//EXERCICIO 2
+//let arrayDeNumbers = [2, 3, 6, 7, 10, 1];
+
+
+//function indiceMaiorValor(arrayDeNumbers) {
+//  let maiorIndex = 0
+//  let maiorNumber = 0;
+//  for (index = 0; index < arrayDeNumbers.length; index += 1) {
+//      if (arrayDeNumbers[index] > maiorNumber) {
+//      maiorNumber = arrayDeNumbers[index];  
+//      maiorIndex = index;
+//    }
+//  }
+//  console.log(maiorIndex);
+//}
+//
+
+//EXERCICIO 2 FEITO APÓS ENTENDER O GABARITO
+
+//function indiceMaiorValor (array) { //criei a function
+//  let indiceMaior = 0 //criei armazenador do maior indice
+//  for (let indice in array) {//criei o for para percorrer o array inteiro
+//    if (array[indiceMaior] < array[indice]) {//criei um comparador que vai avaliar o array na posição guardada e o array na posição do indice
+//      indiceMaior = indice;//se o array na posição guardada for menor que o array na posição do indice, vou substituir o armazenador do maior indice para o indice que possua o maior valor.
+//  }//fecho o meu if
+//  }//fecho o meu for
+//return indiceMaior;//dentro da function, eu retorno o valor que eu queria encontrar
+//}//fecho minha function
+//console.log(indiceMaiorValor([2, 3, 6, 7, 10, 1])); //exibo o resultado da minha função para o array que apresentei.
+
+//EXERCICIO 3
+
+//function indiceMenorValor(array) {
+//  let indiceMenor = 0;
+//  for (let indice in array) {
+//    if (array[indiceMenor] > array[indice]) {
+//      indiceMenor = indice;
+//    }
+//  }
+//return indiceMenor
+//}
+//console.log(indiceMenorValor([2, 4, 6, -7, 10, 0, -3]));
+
+//EXERCICIO 4
+
+function maiorNomeDado(arrayDeNomes) {
+  let indiceMaiorNome = 0;
+  for (let indice in arrayDeNomes) {
+    if (arrayDeNomes[indiceMaiorNome].length < arrayDeNomes[indice].length) {
+      indiceMaiorNome = indice;
+    }
   }
-  else {
-    console.log(false);
-  }
+return arrayDeNomes[indiceMaiorNome];
 }
 
-verificapalindrome('lucas');
-verificapalindrome('arara');
-
+console.log(maiorNomeDado(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
