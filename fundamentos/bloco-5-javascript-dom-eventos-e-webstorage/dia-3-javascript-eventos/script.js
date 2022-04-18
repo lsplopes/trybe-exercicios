52f70c2a994dcfd89d95a82a31905b6908ec9808
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-18 18:09:14 -0300

    resolve exercicio1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,39 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+// Exercicio 1 - O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+//Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+//Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+//Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+const days = document.querySelector('#days');
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+for (let index in dezDaysList) {
+  const addDay = document.createElement('li');
+  addDay.innerText = dezDaysList[index];
+  addDay.className += 'day';
+  days.appendChild(addDay);
+  if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31 ) {
+    addDay.className += ' holiday';
+  }
+  if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25) {
+    addDay.className += ' friday';
+  }
+}
+
+//
