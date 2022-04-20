c62c0493a521c3cc4784b99a980aeede12b693b4
LucasLopes <lopes.sp.lucas@gmail.com>
2022-04-19 23:07:47 -0300

    acrescenta acabamento mediano

--- fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-webstorage/dia-4-javascript-web-storage/script.js
@@ -47,14 +47,17 @@ function updateFontColor() {
   if (fontColorStorage === "black") {
     texto[0].style.color = "black";
     texto[1].style.color = "black";
+    texto[2].style.color = "black";
   } 
   else if (fontColorStorage === "white") {
     texto[0].style.color = "white";
     texto[1].style.color = "white";
+    texto[2].style.color = "white";
   } 
   else {
     texto[0].style.color = "white";
     texto[1].style.color = "white";
+    texto[2].style.color = "white";
     localStorage.setItem('fontColor', "white");    
   }
 }
@@ -65,12 +68,14 @@ blackWhiteButton.addEventListener('click', function(evento) {
     let colorirTexto = document.querySelectorAll('.texto');
     colorirTexto[0].style.color = "white";
     colorirTexto[1].style.color = "white";
+    colorirTexto[2].style.color = "white";
     localStorage.setItem('fontColor', "white");
   }
   else if (evento.target.id === "textBlack") {
     let colorirTexto = document.querySelectorAll('.texto');
     colorirTexto[0].style.color = "black";
     colorirTexto[1].style.color = "black";
+    colorirTexto[2].style.color = "black";    
     localStorage.setItem('fontColor', "black");
   }
 })
@@ -85,18 +90,22 @@ function updateFontSize() {
   if (fontSizeStorage === "80%") {
     texto[0].style.fontSize = "120%";
     texto[1].style.fontsize = "80%";
+    texto[2].style.fontSize = "120%";
   } 
   else if (fontSizeStorage === "100%") {
     texto[0].style.fontSize = "140%";
     texto[1].style.fontSize = "100%";
+    texto[2].style.fontSize = "140%";
   } 
   else if (fontSizeStorage === "120%") {
     texto[0].style.fontSize = "160%";
     texto[1].style.fontSize = "120%";
+    texto[2].style.fontSize = "160%";
   } 
   else {
     texto[0].style.fontSize = "140%";
     texto[1].style.fontSize = "100%";
+    texto[2].style.fontSize = "140%";
     localStorage.setItem('fontSize', "100%");    
   }
 }
@@ -107,18 +116,21 @@ sizeButtons.addEventListener('click', function(evento) {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.fontSize = "120%";
     redimensionarTexto[1].style.fontSize = "80%";
+    redimensionarTexto[2].style.fontSize = "120%";
     localStorage.setItem('fontSize', "80%");
   }
   else if (evento.target.id === "mediumSize") {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.fontSize = "140%";
     redimensionarTexto[1].style.fontSize = "100%";
+    redimensionarTexto[2].style.fontSize = "140%";
     localStorage.setItem('fontSize', "100%");
   }
   else if (evento.target.id === "largeSize") {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.fontSize = "160%";
     redimensionarTexto[1].style.fontSize = "120%";
+    redimensionarTexto[2].style.fontSize = "160%";
     localStorage.setItem('fontSize', "120%");
   }
 });
@@ -133,18 +145,22 @@ function updateLineHeight() {
   if (lineHeigthStorage === "101%") {
     texto[0].style.lineHeight = "160%";
     texto[1].style.lineHeight = "101%";
+    texto[2].style.lineHeight = "101%";
   } 
   else if (lineHeigthStorage === "130%") {
     texto[0].style.lineHeight = "140%";
     texto[1].style.lineHeight = "130%";
+    texto[2].style.lineHeight = "130%";
   } 
   else if (lineHeigthStorage === "160%") {
     texto[0].style.lineHeight = "160%";
     texto[1].style.lineHeight = "160%";
+    texto[2].style.lineHeight = "160%";
   } 
   else {
     texto[0].style.lineHeight = "140%";
     texto[1].style.lineHeight = "130%";
+    texto[2].style.lineHeight = "130%";
     localStorage.setItem('lineHeight', "130%");    
   }
 }
@@ -155,18 +171,21 @@ lineHeightButtons.addEventListener('click', function(evento) {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.lineHeight = "160%";
     redimensionarTexto[1].style.lineHeight = "101%";
+    redimensionarTexto[2].style.lineHeight = "101%";
     localStorage.setItem('lineHeight', "101%");
   }
   else if (evento.target.id === "lineHeigthMedium") {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.lineHeight = "140%";
     redimensionarTexto[1].style.lineHeight = "130%";
+    redimensionarTexto[2].style.lineHeight = "130%";
     localStorage.setItem('lineHeight', "130%");
   }
   else if (evento.target.id === "lineHeigthLarge") {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.lineHeight = "160%";
     redimensionarTexto[1].style.lineHeight = "160%";
+    redimensionarTexto[2].style.lineHeight = "160%";
     localStorage.setItem('lineHeight', "160%");
   }
 });
@@ -181,18 +200,22 @@ function updateFontFamily() {
   if (textFontStorage === "Arial") {
     textFont[0].style.fontFamily = "Arial";
     textFont[1].style.fontFamily = "Arial";
+    textFont[2].style.fontFamily = "Arial";
   } 
   else if (textFontStorage === "Monospace") {
     textFont[0].style.fontFamily = "Monospace";
     textFont[1].style.fontFamily = "Monospace";
+    textFont[2].style.fontFamily = "Monospace";
   } 
   else if (textFontStorage === "Verdana") {
     textFont[0].style.fontFamily = "Verdana";
-    textFont[1].style.fontFamily = "Verdana";    
+    textFont[1].style.fontFamily = "Verdana";
+    textFont[2].style.fontFamily = "Verdana";    
   } 
   else {
     textFont[0].style.fontFamily = "Arial";
     textFont[1].style.fontFamily = "Arial";
+    textFont[2].style.fontFamily = "Arial";
     localStorage.setItem('textFamilyFont', "Arial");
   }
 }
@@ -208,6 +231,7 @@ fontFamilyButton.addEventListener('click', function() {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.fontFamily = "Monospace";
     redimensionarTexto[1].style.fontFamily = "Monospace";
+    redimensionarTexto[2].style.fontFamily = "Monospace";
     localStorage.setItem('textFamilyFont', "Monospace");
     console.log('arial mono')
   }
@@ -215,6 +239,7 @@ fontFamilyButton.addEventListener('click', function() {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.fontFamily = "Verdana";
     redimensionarTexto[1].style.fontFamily = "Verdana";
+    redimensionarTexto[2].style.fontFamily = "Verdana";
     localStorage.setItem('textFamilyFont', "Verdana");
     console.log('mono verda')
   }
@@ -222,6 +247,7 @@ fontFamilyButton.addEventListener('click', function() {
     let redimensionarTexto = document.querySelectorAll('.texto');
     redimensionarTexto[0].style.fontFamily = "Arial";
     redimensionarTexto[1].style.fontFamily = "Arial";
+    redimensionarTexto[2].style.fontFamily = "Arial";
     localStorage.setItem('textFamilyFont', "Arial");
     console.log('verda ari')
   }
