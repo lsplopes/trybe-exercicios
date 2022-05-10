995056b739f0f5e54a7d0b9a90b281d0f0fc2a56
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-09 21:33:59 -0300

    resolve até o exercicio 5 da parte 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/parte3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/parte3.js
@@ -18,10 +18,25 @@ const lesson3 = {
   turno: 'noite',
 };
 
-let noiteDois = (objeto1, chave1, valor) => {
-  const chaveA = chave1;
-  const valorA = valor;
-  objeto1[chaveA] = valorA;
-  return objeto1;
+const noiteDois = (objeto1, chave1, valor) => {
+ const chaveA = chave1;
+ const valorA = valor;
+ objeto1[chaveA] = valorA;
+ return objeto1;
 }
 console.log(noiteDois(lesson2, 'turno', 'noite'));
+
+const listKeys = (objeto) => console.log(Object.keys(objeto));
+listKeys(lesson1);
+
+const keyLength = (objeto) => console.log(Object.keys(objeto).length);
+keyLength(lesson1);
+
+const listValues = (objeto) => console.log(Object.values(objeto));
+listValues(lesson1);
+
+// EXERCICIO 5
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3});
+
+console.log(allLessons);
