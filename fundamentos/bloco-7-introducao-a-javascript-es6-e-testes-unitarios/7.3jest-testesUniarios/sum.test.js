e3b73f25ae15a93c686f845ba207fb13c4fad384
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-10 19:12:00 -0300

    resolve exercicio 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.3jest-testesUniarios/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.3jest-testesUniarios/sum.test.js
@@ -1,7 +1,20 @@
 const sum = require('./sum');
 
 describe('Testes da função sum', () => {
+
   it('Somando 4 + 5 igual a 9', () => {
     expect(sum(4, 5)).toBe(9);
+  });
+
+  it('Somando 0 + 0 igual a 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('Lança um erro quando os parâmetros são 4 e "5"(string 5)', () => {
+    expect(() => sum(4, "5")).toThrowError();
+  });
+  
+  it('Testa se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => sum(4, "5")).toThrowError('parameters must be numbers');
   });
 })
