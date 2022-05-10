df7879a79a1ebf3cddc4768245487f3c89609d14
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-09 22:26:03 -0300

    resolve parte 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/parte3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/parte3.js
@@ -24,19 +24,58 @@ const noiteDois = (objeto1, chave1, valor) => {
  objeto1[chaveA] = valorA;
  return objeto1;
 }
-console.log(noiteDois(lesson2, 'turno', 'noite'));
+// console.log(noiteDois(lesson2, 'turno', 'noite'));
 
 const listKeys = (objeto) => console.log(Object.keys(objeto));
-listKeys(lesson1);
+// listKeys(lesson1);
 
 const keyLength = (objeto) => console.log(Object.keys(objeto).length);
-keyLength(lesson1);
+// keyLength(lesson1);
 
 const listValues = (objeto) => console.log(Object.values(objeto));
-listValues(lesson1);
+// listValues(lesson1);
 
 // EXERCICIO 5
 
 const allLessons = Object.assign({}, { lesson1, lesson2, lesson3});
 
-console.log(allLessons);
+// console.log(allLessons);
+
+//EXERCICIO 6
+
+const totalStudents = (object) => {
+  const lesson1 = Object.values(object.lesson1);
+  const lesson2 = Object.values(object.lesson2);
+  const lesson3 = Object.values(object.lesson3);
+  const totalStudent = lesson1[1] + lesson2[1] + lesson3[1];
+  return totalStudent
+}
+// console.log(totalStudents(allLessons));
+
+//EXERCICIO 7
+
+const valueByPosition = (lesson, position) => {
+  const lessonSearch = Object.values(lesson);
+  const output = lessonSearch[position];
+  return output  
+}
+// console.log(valueByPosition(lesson1, 0));
+
+//EXERCICIO 8
+
+const verifyPair = (objeto, chave, valor) => {
+  const arrayObject = Object.entries(objeto);
+  for (let index = 0; index < arrayObject.length; index += 1) {
+    console.log(arrayObject[index][0]);
+    console.log(arrayObject[index][1]);
+    if (arrayObject[index][0] === chave && arrayObject[index][1] === valor) {
+      result = true;
+      break;
+    } else {
+      result = false;
+    }
+  }
+  return result
+}
+// console.log(verifyPair(lesson3, 'turno', 'noite'));
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
