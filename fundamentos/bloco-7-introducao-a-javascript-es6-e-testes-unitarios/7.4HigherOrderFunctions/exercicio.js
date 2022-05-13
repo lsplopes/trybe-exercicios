92fddf69b1ed2c7637e4f6f9d57f63863f8ddbd9
LucasLopes <lopes.sp.lucas@gmail.com>
2022-05-13 14:52:34 -0300

    resolve exercicios 1 e 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.4HigherOrderFunctions/exercicio.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/7.4HigherOrderFunctions/exercicio.js
@@ -1,8 +1,36 @@
-const newEmployees = () => {
+//EXERCICIO 1
+
+const newEmployees = (func) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+
+const cadastro = (name) => {
+  const removeSpaces = name.toLowerCase().replace(/ /g, '_')
+  const email = `${removeSpaces}@trybe.com.br`
+  const objeto = {
+    nomeCompleto: name,
+    email: email,
+  }
+  return objeto
+}
+
+// console.log(newEmployees(cadastro));
+
+//EXERCICIO 2
+
+const resultado = (numero, sortfunc) => {
+  if (numero === sortfunc) {
+    return 'Parabéns você ganhou!';
+  } else {
+    return 'Tente novamente.'
+  }
+} 
+
+const sortfunc = () => Math.floor(Math.random() * 5 + 1);
+
+// console.log(resultado(3, sortfunc()));
