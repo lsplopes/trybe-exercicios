9eb8c5adf993fc8e5d20581d24d3a6803601e1fe
LucasLopes <lopes.sp.lucas@gmail.com>
2022-06-01 19:38:18 -0300

    resolve exercicio bonus

--- front-end/bloco-9-javascript-e-testes-assincronos/9.2-JavaScript-Assincrono-FetchAPI-e-AsyncAwait/02script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/9.2-JavaScript-Assincrono-FetchAPI-e-AsyncAwait/02script.js
@@ -1,19 +1,19 @@
+let brl = 0;
+
 const append = (data) => {
   const listaOrdenada = document.getElementById('coinList');
 
   const linha = document.createElement('li');
   const div = document.createElement('div');
 
-  div.innerHTML = `${data.name} (${data.symbol}): ${Number(data.priceUsd).toFixed(2)}.`
+  div.innerHTML = `${data.name} (${data.symbol}): ${(Number(data.priceUsd)*brl).toFixed(2)}.`
 
   linha.appendChild(div);
   listaOrdenada.appendChild(linha);
 }
 
 const catchTenFirst = (data) => {
-  console.log(data);
   const arrayComDez = data.data.slice(0, 10);
-  console.log(arrayComDez);
   return arrayComDez.forEach((coin) => append(coin));
 }
 
@@ -28,4 +28,15 @@ const fetchcoin = async () => {
   }
 }
 
-fetchcoin()
+const fetchConvert = async () => {
+  const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json'
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    brl = data.usd.brl;
+  } catch(error) {
+    console.log(`algo errado não está certo com a conversão em ( \n${error}`);
+  }
+}
+fetchConvert();
+fetchcoin();
