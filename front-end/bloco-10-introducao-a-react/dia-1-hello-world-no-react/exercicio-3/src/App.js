c9fa95e7dc81beb33cc66afa5c9320490d18347c
LucasLopes <lopes.sp.lucas@gmail.com>
2022-06-10 15:55:20 -0300

    resolve o footer e estiliza utilizando estilização dada no gabarito

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Content from './Content';
+import Footer from './Footer';
 import Header from './Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header/>
       <Content/>
+      <Footer/>
     </div>
   );
 }
